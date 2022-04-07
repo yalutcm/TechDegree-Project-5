2d763beeb1ed076ef8e567ed6d4ba5e04bc0aad6
yalutcm <tulaychrism@gmail.com>
2022-04-07 08:51:14 -0400

    Project 5: Interactive Photo Gallery
    - Rewriting using SASS

---
+++ js/app.js
@@ -0,0 +1,2 @@
+
+baguetteBox.run('.gallery');
---
+++ js/searchGallery.js
@@ -0,0 +1,19 @@
+
+function searchGallery() {
+    let input = document.getElementById('search'); 
+    input = input.value.toUpperCase();
+    let sgallery = document.getElementsByClassName('thumbnails');
+
+     // Loop through all list items,
+    for (let i = 0; i < sgallery.length; i++) {
+        let entry = sgallery[i].getAttribute('data-caption');
+        entry = entry.toUpperCase();
+
+        if (entry.includes(input)) {
+            sgallery[i].style.display = 'inherit';
+        } else {
+            sgallery[i].style.display = 'none';
+        }
+    }
+}
+
