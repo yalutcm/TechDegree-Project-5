78555418dc763ac5556c29a8f149f46cb991b881
yalutcm <tulaychrism@gmail.com>
2022-04-09 13:19:54 -0400

    Grid not flexable
    - Need to fix Grid layout so that it is flexable with different screen sizes

--- js/searchGallery.js
+++ js/searchGallery.js
@@ -17,3 +17,24 @@ function searchGallery() {
     }
 }
 
+// function searchGalleryWhile() { 
+//     let input = document.getElementById('search'); 
+//     input = input.value.toUpperCase();
+//     let sgallery = document.getElementsByClassName('thumbnails');
+//     var shouldContinue = 0
+
+//     do {
+//         let entry = sgallery[i].getAttribute('data-caption');
+//         entry = entry.toUpperCase();
+
+//         if (entry.includes(input)) {
+//             sgallery[i].style.display = 'inherit';
+//         } else {
+//             sgallery[i].style.display = 'none';
+//         }
+//         shouldContinue += 1
+//     } while (shouldContinue < sgallery.length)
+
+// }
+
+
