4880c4ea1399f3aa7290cb934de916e26ac1df59
yalutcm <tulaychrism@gmail.com>
2022-07-12 21:18:40 -0700

    Final Check
    CSS + HTML Validated
    Verified functionality

--- js/searchGallery.js
+++ js/searchGallery.js
@@ -1,10 +1,10 @@
-
 function searchGallery() {
     let input = document.getElementById('search'); 
     input = input.value.toUpperCase();
     let sgallery = document.getElementsByClassName('thumbnails');
 
-     // Loop through all list items,
+     // Loop through thumbnail images
+     // Match images with keyword search
     for (let i = 0; i < sgallery.length; i++) {
         let entry = sgallery[i].getAttribute('data-caption');
         entry = entry.toUpperCase();
@@ -16,25 +16,3 @@ function searchGallery() {
         }
     }
 }
-
-// function searchGalleryWhile() { 
-//     let input = document.getElementById('search'); 
-//     input = input.value.toUpperCase();
-//     let sgallery = document.getElementsByClassName('thumbnails');
-//     var shouldContinue = 0
-
-//     do {
-//         let entry = sgallery[i].getAttribute('data-caption');
-//         entry = entry.toUpperCase();
-
-//         if (entry.includes(input)) {
-//             sgallery[i].style.display = 'inherit';
-//         } else {
-//             sgallery[i].style.display = 'none';
-//         }
-//         shouldContinue += 1
-//     } while (shouldContinue < sgallery.length)
-
-// }
-
-
